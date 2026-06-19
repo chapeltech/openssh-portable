d68319c971ab18fd7b0d803c6e6843458160542c
Roland C. Dowdeswell <elric@imrryr.org>
2026-06-19 19:19:15 +0100

    Restrict Windows sshd host key in GSS KEX test

--- .github/scripts/gsskex-wsl-interop.ps1
+++ .github/scripts/gsskex-wsl-interop.ps1
@@ -232,6 +232,10 @@ function Start-WindowsSshd([string]$AuthorizedKeys) {
     $hostKey = Join-Path $testRoot 'windows-ssh-host-ed25519'
     Remove-Item "$hostKey*" -Force -ErrorAction SilentlyContinue
     Invoke-Checked $sshKeygen @('-q', '-t', 'ed25519', '-N', '', '-f', $hostKey)
+    Invoke-Checked icacls.exe @($hostKey, '/inheritance:r')
+    Invoke-Checked icacls.exe @($hostKey, '/grant:r',
+        '*S-1-5-18:F', '*S-1-5-32-544:F')
+    Invoke-Checked icacls.exe @($hostKey, '/setowner', '*S-1-5-32-544')
 
     $cfg = Join-Path $testRoot 'windows_sshd_config'
     $serverLog = Join-Path $logRoot 'windows-sshd.log'
